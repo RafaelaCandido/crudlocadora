7abfd7fe5b1b4e29081d4098b1156f0021b33229
Rafaela de Jesus Alves Candido <CIANDT\rcandido@lnb027960cps.ciandt.global>
2021-07-25 22:46:30 -0300

    criação da classe de modelo cliente

---
+++ src/main/java/br/com/ciandt/crudlocadora/model/Cliente.java
@@ -0,0 +1,25 @@
+package br.com.ciandt.crudlocadora.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = {"cpf"}))
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String cpf;
+    private String telefone;
+    private String endereco;
+}
